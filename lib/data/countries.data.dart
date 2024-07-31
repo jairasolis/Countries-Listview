725e8c0aeb73bee023512231a97f41bc934f73e5
jairasolis <jairasolis030@gmail.com>
2024-07-31 13:03:05 +0800

    added the countries data

---
+++ lib/data/countries.data.dart
@@ -0,0 +1,54 @@
+import 'package:countries_listview/models/countries.dart';
+
+const countriesData = [
+  CountryModel(
+      countryName: 'Azerbaijan',
+      flag: 'assets/images/flags/azerbaijan.webp',
+      language: 'Azerbaijani',
+      continent: 'Asia'),
+  CountryModel(
+      countryName: 'Sri Lanka',
+      flag: 'assets/images/flags/sri lanka.png',
+      language: 'Sinhala, Tamil',
+      continent: 'Asia'),
+  CountryModel(
+      countryName: 'New Zealand',
+      flag: 'assets/images/flags/new zealand.png',
+      language: 'English, Māori',
+      continent: 'Oceania'),
+  CountryModel(
+      countryName: 'Vanuatu',
+      flag: 'assets/images/flags/vanuatu.webp',
+      language: 'Bislama, English, French',
+      continent: 'Oceania'),
+  CountryModel(
+      countryName: 'Senegal',
+      flag: 'assets/images/flags/senegal.webp',
+      language: 'French',
+      continent: 'Africa'),
+  CountryModel(
+      countryName: 'Fiji',
+      flag: 'assets/images/flags/fiji.webp',
+      language: 'English, Fijian, Hindi',
+      continent: 'Oceania'),
+  CountryModel(
+      countryName: 'Costa Rica',
+      flag: 'assets/images/flags/costa rica.webp',
+      language: 'Spanish',
+      continent: 'North America'),
+  CountryModel(
+      countryName: 'Zimbabwe',
+      flag: 'assets/images/flags/zimbabwe.png',
+      language: 'English, Shona, Ndebele',
+      continent: 'Africa'),
+  CountryModel(
+      countryName: 'Liechtenstein',
+      flag: 'assets/images/flags/liechtenstein.webp',
+      language: 'German',
+      continent: 'Europe'),
+  CountryModel(
+      countryName: 'Bulgaria',
+      flag: 'assets/images/flags/bulgaria.webp',
+      language: 'Bulgarian',
+      continent: 'Europe'),
+];
